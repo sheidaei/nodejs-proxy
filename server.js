f4e8e296f9c8747e7fccf95a1d38aea70b809e61
sheidaei <01@sheidaei.com>
2018-01-21 21:28:42 -0500

    Update server.js
    dynamic port

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");  
 const request = require('request');  
 const app = express();
+var port = process.env.PORT || 8080;
 
 app.get('/', (req, res) =>  request({  
     url: req.query.url,
     method: req.query.method
-  }).pipe(res);
-);
-app.listen(8080); 
+  }).pipe(res));
+app.listen(port); 
